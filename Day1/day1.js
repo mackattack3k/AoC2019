d93af37094042628555a5e89b3aaeaaaa00b070c
mackattack3k <marcushanssonmh@gmail.com>
2019-12-01 12:32:56 +0100

    Day 1 part 2

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,9 +1,16 @@
+let part2 = 0;
 const input = require('fs').readFileSync("Day1/input", 'utf-8')
     .split('\n')
     .filter(Boolean)
     .reduce((totalFuel, fuel) => {
       const requiredFuel =  Math.floor(parseInt(fuel) / 3) - 2;
+      let totalRequiredFuel = requiredFuel;
+      while (totalRequiredFuel > 0) {
+        part2 = part2 + totalRequiredFuel;
+        totalRequiredFuel = Math.floor(totalRequiredFuel / 3) - 2;
+      }
       return totalFuel + requiredFuel;
     }, 0);
 
-console.log(input);
+console.log('part 1: ', input);
+console.log('part 2:', part2);
